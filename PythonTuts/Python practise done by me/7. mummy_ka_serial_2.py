48bcb79742cde7f7bc15f524fa4c1604bcb81446
Anant <85330429+anant-harryfan@users.noreply.github.com>
2023-10-16 01:32:32 +0530

    7. nothing

--- PythonTuts/Python practise done by me/7. mummy_ka_serial_2.py
+++ PythonTuts/Python practise done by me/7. mummy_ka_serial_2.py
@@ -1,8 +1,1 @@
-import time
-serial = ["ghum hia kisi ke pyarr me", "anupama", "ye rishta kya kaha rahata hai"]
-print(serial[0])
-time.sleep(3)
-print(serial[1])
-time.sleep(3)
-print(serial[2])
-print("Ye sadi si cheez khatam")
+
